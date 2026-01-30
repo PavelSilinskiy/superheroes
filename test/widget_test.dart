1a8439822bc8b884456263ca62722c114dda7378
PavelSilinskiy <pavel.silinskiy.03@mail.ru>
2026-01-31 02:52:43 +0300

    test: test 2 passed

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,7 +8,7 @@ import 'lesson_1/test_lesson_1_task_5.dart';
 
 void main() {
   // group("l05h01", () => runTestLesson1Task1());
-  // group("l05h02", () => runTestLesson1Task2());
+  group("l05h02", () => runTestLesson1Task2());
   // group("l05h03", () => runTestLesson1Task3());
   // group("l05h04", () => runTestLesson1Task4());
   // group("l05h05", () => runTestLesson1Task5());
