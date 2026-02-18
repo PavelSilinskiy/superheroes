79d7442547ab5627c4155d5cc94fa85c36a42449
PavelSilinskiy <pavel.silinskiy.03@mail.ru>
2026-02-18 10:02:55 +0300

    feat: Implement storage methods

--- lib/favorite_superheroes_storage.dart
+++ lib/favorite_superheroes_storage.dart
@@ -1,32 +1,61 @@
+import 'dart:convert';
+
 import 'package:superheroes/model/superhero.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class FavoriteSuperheroesStorage {
+  static const _key = "favorite_superheroes";
+
+  Future<List<String>> _getRawSuperheroes() async {
+    final SharedPreferences sp = await SharedPreferences.getInstance();
+    return sp.getStringList(_key) ?? [];
+  }
+
+  Future<bool> _setRawSuperheroes(List<String> rawSuperheroes) async {
+    final SharedPreferences sp = await SharedPreferences.getInstance();
+    return sp.setStringList(_key, rawSuperheroes);
+  }
+
+  Future<List<Superhero>> _getSuperheroes() async {
+    final rawSuperheroes = await _getRawSuperheroes();
+    final List<Superhero> superheroes = rawSuperheroes
+        .map<Superhero>(
+          (rawSuperhero) => Superhero.fromJson(json.decode(rawSuperhero)),
+        )
+        .toList();
+    return superheroes;
+  }
+
+  Future<bool> _setSuperheroes(List<Superhero> superheroes) async {
+    final List<String> rawSuperheroes = superheroes
+        .map<String>((superhero) => json.encode(superhero.toJson()))
+        .toList();
+    return _setRawSuperheroes(rawSuperheroes);
+  }
 
   Future<bool> addToFavorites(final Superhero superhero) async {
-    //TODO:
-    throw UnimplementedError();
+    final rawSuperheroes = await _getRawSuperheroes();
+    rawSuperheroes.add(json.encode(superhero.toJson()));
+    return await _setRawSuperheroes(rawSuperheroes);
   }
 
   Future<bool> removeFromFavorites(final String id) async {
-    //TODO:
-    throw UnimplementedError();
+    final superheroes = await _getSuperheroes();
+    superheroes.removeWhere((item) => (item.id == id));
+    return await _setSuperheroes(superheroes);
   }
 
   Future<Superhero> getSuperhero(final String id) async {
+    final List<Superhero> superheroes = await _getSuperheroes();
+    return superheroes.firstWhere((superhero) => (superhero.id == id));
+  }
+
+  Stream<List<Superhero>> observeFavoriteSuperheroes() {
+    return _getSuperheroes().asStream();
+  }
+
+  Stream<bool> observeIsFavorite(final String id) {
     //TODO:
     throw UnimplementedError();
   }
-
-  Stream<List<Superhero>> observeFavoriteSuperheroes () {
-    //TODO:
-    throw UnimplementedError();
-  }
-
-  Stream<bool> observeIsFavorite (final String id) {
-    //TODO:
-    throw UnimplementedError();
-  }
-
-
-  
 }
